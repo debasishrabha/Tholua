f8424c1a18df818c908f0e04c99dd604b14296bd
debasish69 <debasishrva78@gmail.com>
2025-09-04 09:58:50 +0530

    update

--- public/javascript/config.js
+++ public/javascript/config.js
@@ -1,6 +1,11 @@
+// config.js
 const API_URL =
     window.location.hostname === "localhost"
         ? "http://localhost:5000/api"
         : `${window.location.origin}/api`;
 
-const token = localStorage.getItem("token");
+// Export the variables
+export { API_URL };
+
+// Helper function to get token
+export const getToken = () => localStorage.getItem("token");
