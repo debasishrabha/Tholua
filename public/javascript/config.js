d5171807d7dc012094654b1d8064bcb018e0024d
debasish69 <debasishrva78@gmail.com>
2025-09-04 09:22:41 +0530

    update

--- public/javascript/config.js
+++ public/javascript/config.js
@@ -1,6 +1,6 @@
-// config.js
-const API_URL = window.location.hostname === "localhost"
-    ? "http://localhost:5000/api"
-    : "https://tholua-1.onrender.com/api";
+const API_URL =
+    window.location.hostname === "localhost"
+        ? "http://localhost:5000/api"
+        : `${window.location.origin}/api`;
 
 const token = localStorage.getItem("token");
